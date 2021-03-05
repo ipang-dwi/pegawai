f26bb851fba9437ecd3403892726647485109249
Ipang Dwi <admin@firstplato.com>
2021-03-05 08:18:38 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
 	// (nama server, user, password, database)
-	$mysqli = new mysqli("localhost", "root", "1717", "pegawai");
+	$mysqli = new mysqli("localhost", "root", "", "pegawai");
 	/* check connection */
 	if (mysqli_connect_errno()) {
 		printf("Connect failed: %s\n", mysqli_connect_error());
